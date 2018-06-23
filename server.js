5843838b01feb7247fbbd9373898e93ec13b0d19
emitdlr <emit@utexas.edu>
2018-06-23 08:48:21 -0500

    much changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configure body parser for AJAX requests
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Serve up static assets
+
 app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
